0321c3e818a26cd81c98bc3234278979f4a27891
tomasrpita <tomasrpita@gmail.com>
2024-08-11 11:15:35 +0200

    Add content to course modules

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,4 +1,6 @@
+from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponseMixin, View
@@ -6,7 +8,15 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
 from .forms import ModuleFormSet
-from .models import Course
+from .models import Content, Course, Module
+
+# class ManageCourseListView(ListView):
+#     model = Course
+#     template_name = "courses/manage/course/list.html"
+
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         return qs.filter(owner=self.request.user)
 
 
 class OwnerMixin:
@@ -44,6 +54,12 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     permission_required = "courses.change_course"
 
 
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
+
+
+# CourseModuleCreateUpdateView??
 class CourseModuleUpdateView(TemplateResponseMixin, View):
     template_name = "courses/manage/module/formset.html"
     course = None
@@ -67,15 +83,55 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
         return self.render_to_response({"course": self.course, "formset": formset})
 
 
-class CourseDeleteView(OwnerCourseMixin, DeleteView):
-    template_name = "courses/manage/course/delete.html"
-    permission_required = "courses.delete_course"
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = "courses/manage/content/form.html"
+
+    def get_model(self, model_name):
+        if model_name in ["text", "video", "image", "file"]:
+            return apps.get_model(app_label="courses", model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(
+            model, exclude=["owner", "order", "created", "updated"]
+        )
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user
+        )
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
+        return super().dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({"form": form, "object": self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(
+            self.model, instance=self.obj, data=request.POST, files=request.FILES
+        )
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                # new content
+                Content.objects.create(module=self.module, item=obj)
+            return redirect("module_content_list", self.module.id)
+        return self.render_to_response({"form": form, "object": self.obj})
 
 
-# class ManageCourseListView(ListView):
-#     model = Course
-#     template_name = "courses/manage/course/list.html"
-
-#     def get_queryset(self):
-#         qs = super().get_queryset()
-#         return qs.filter(owner=self.request.user)
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(Content, id=id, module__course__owner=request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect("module_content_list", module.id)
