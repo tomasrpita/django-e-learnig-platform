4f8cd6bf42ec8de079fc04917fd8cdd99882f8a8
tomasrpita <tomasrpita@gmail.com>
2024-08-13 05:43:06 +0200

    Add course views to use class-based views and add owner mixins

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,15 +1,17 @@
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Count
 from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .forms import ModuleFormSet
-from .models import Content, Course, Module
+from .models import Content, Course, Module, Subject
 
 # class ManageCourseListView(ListView):
 #     model = Course
@@ -160,3 +162,23 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
                 order=order
             )
         return self.render_json_response({"saved": "OK"})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response(
+            {"subjects": subjects, "subject": subject, "courses": courses}
+        )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -20,12 +20,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from courses.views import CourseListView
 
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
     path("course/", include("courses.urls")),
+    path("", CourseListView.as_view(), name="course_list"),
 ]
 
 
