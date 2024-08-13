f812e0751bdb966cafc1c9141051468834e6b61f
tomasrpita <tomasrpita@gmail.com>
2024-08-13 10:47:21 +0200

    Add content rendering in ItemBase model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.template.loader import render_to_string
 from django.db import models
 from .fields import OrderField
 
@@ -67,6 +68,11 @@ class ItemBase(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def render(self):
+        return render_to_string(
+            f"courses/content/{self._meta.model_name}.html", {"item": self}
+        )
 
     class Meta:
         abstract = True
