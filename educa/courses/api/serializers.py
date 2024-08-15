2f2d21cee3237db5f67b302b70c9bf84a262cca7
tomasrpita <tomasrpita@gmail.com>
2024-08-15 10:03:13 +0200

    Refactor SubjectSerializer to include total_courses and popular_courses fields

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,10 +1,20 @@
+from django.db.models import Count
 from rest_framework import serializers
 from courses.models import Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
+    popular_courses = serializers.SerializerMethodField()
+
+    def get_popular_courses(self, obj):
+        courses = obj.courses.annotate(total_students=Count("students")).order_by(
+            "-total_students"
+        )[:3]
+        # con un supuesto serializer de Course
+        # return CourseSerializer(courses, many=True).data
+        return [f"{c.title} ({c.total_students})" for c in courses]
 
     class Meta:
         model = Subject
-        fields = ["id", "title", "slug", "total_courses"]
+        fields = ["id", "title", "slug", "total_courses", "popular_courses"]
