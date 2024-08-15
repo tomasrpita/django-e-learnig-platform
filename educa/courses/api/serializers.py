f12ee2107ff90dcef0ebba261d70d16b0a1cbfe2
tomasrpita <tomasrpita@gmail.com>
2024-08-15 09:53:26 +0200

    Refactor SubjectSerializer to include total_courses field

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -3,6 +3,8 @@ from courses.models import Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
+    total_courses = serializers.IntegerField()
+
     class Meta:
         model = Subject
-        fields = ["id", "title", "slug"]
+        fields = ["id", "title", "slug", "total_courses"]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
+from django.db.models import Count
 from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
 
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
+    # queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
+
     serializer_class = SubjectSerializer
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    # queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
+
     serializer_class = SubjectSerializer
