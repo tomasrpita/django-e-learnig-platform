8e6e1c6db4194fbb0cfd899a303fae42a647eeff
tomasrpita <tomasrpita@gmail.com>
2024-08-16 18:31:21 +0200

    Add class CourseEnrollView from APIView

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,10 +1,12 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
-
 
 # class SubjectListView(generics.ListAPIView):
 #     # queryset = Subject.objects.all()
@@ -31,3 +33,10 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related("modules")
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({"enrolled": True})
