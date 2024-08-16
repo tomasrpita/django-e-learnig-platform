9fad063ee3c775b0ea064e626552e0f7c08dbcac
tomasrpita <tomasrpita@gmail.com>
2024-08-16 18:42:24 +0200

    Add authentication for CourseEnrollView to API URLs

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -36,6 +38,9 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
