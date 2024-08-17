656df7700ecb969cb2454af4e1044cb4e1f37242
tomasrpita <tomasrpita@gmail.com>
2024-08-17 05:31:48 +0200

    Add CourseEnrollView and custom permissions

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Course, Module, Subject
+from courses.models import Content, Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -29,6 +29,44 @@ class ModuleSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     # modules = serializers.StringRelatedField(many=True, read_only=True)
     modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ["order", "item"]
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description", "contents"]
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
 
     class Meta:
         model = Course
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -2,14 +2,18 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import action
-
 
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import (
+    CourseSerializer,
+    CourseWithContentsSerializer,
+    SubjectSerializer,
+)
 from courses.models import Course, Subject
 
 # class SubjectListView(generics.ListAPIView):
@@ -48,6 +52,16 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 # Esta implementada arriba en el viewset
