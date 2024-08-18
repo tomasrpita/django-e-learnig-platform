8af3cba8f6d9a73e4a3f34c76cfaf0a34fed12dc
tomasrpita <tomasrpita@gmail.com>
2024-08-18 13:30:22 +0200

    Add WebSocket support for chat functionality

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -1,12 +1,19 @@
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
 
 django_asgi_app = get_asgi_application()
+
+# import is below is needed to ensure the application
+# registry is populated before importing code that may import ORM models.
+from chat.routing import websocket_urlpatterns  # noqa
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
