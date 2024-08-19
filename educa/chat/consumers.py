c4c54139a01b03f11d784549acd9a424ca81f3d3
tomasrpita <tomasrpita@gmail.com>
2024-08-19 07:04:45 +0200

    Refactor ChatConsumer to include user and datetime in chat messages

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,10 +1,12 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.id}"
         # join room group
@@ -28,7 +30,13 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json["message"]
         # send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "datetime": timezone.now().isoformat(),
+            },
         )
         # send message to WebSocket
         # self.send(text_data=json.dumps({"message": message}))
