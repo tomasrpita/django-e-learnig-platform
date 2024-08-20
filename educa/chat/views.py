66104396a90c0d6b3d88c96de6a9917a03ff2b41
tomasrpita <tomasrpita@gmail.com>
2024-08-20 06:37:52 +0200

    Refactor chat room template to display latest messages

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -9,7 +9,17 @@ def course_chat_room(request, course_id):
     try:
         # retrieve course with given id joined by the current user
         course = request.user.courses_joined.get(id=course_id)
+        # retrieve chat history
+        latest_messages = course.chat_messages.select_related("user").order_by("-id")[
+            :5
+        ]
+        latest_messages = reversed(latest_messages)
+
     except Course.DoesNotExist:
         # user is not a student of the course or course does not exist
         return HttpResponseForbidden()
-    return render(request, "chat/room.html", {"course": course})
+    return render(
+        request,
+        "chat/room.html",
+        {"course": course, "latest_messages": latest_messages},
+    )
