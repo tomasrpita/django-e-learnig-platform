ccd66eec04d2778e8cc52986b6e11b007d5f00e5
tomasrpita <tomasrpita@gmail.com>
2024-08-24 16:03:58 +0200

    Refactor project dependencies and Dockerfile, and update project settings for local and production environments

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -8,6 +8,6 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        NAME: BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
