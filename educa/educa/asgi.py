db2c7a052e3129f6d9b9fcd4c5e7042a72374365
tomasrpita <tomasrpita@gmail.com>
2024-08-26 06:36:41 +0200

    Refactor .gitignore to exclude uWSGI socket file and add Daphe config

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -1,6 +1,7 @@
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
@@ -14,6 +15,9 @@ from chat.routing import websocket_urlpatterns  # noqa
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        # "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
